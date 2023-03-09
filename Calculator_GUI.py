3e73bb68b7ce23dab7e191b2077d7826a573a408
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-03-08 20:59:11 -0700

    Bug fix.

--- Calculator_GUI.py
+++ Calculator_GUI.py
@@ -7,7 +7,7 @@ window = sg.Window(title="Calculator", layout=layout, margins=(100, 50))
 
 while True:
     event, values = window.read()
-    if event == "OK" or even == sg.WIN_CLOSED:
+    if event == "OK" or event == sg.WIN_CLOSED:
         break
 
 window.close()
