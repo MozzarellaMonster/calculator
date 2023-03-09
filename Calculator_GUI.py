a60a2fe17b1da1d8d0f2f107be69f61593cfb770
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-03-08 20:58:16 -0700

    Added basic window

--- Calculator_GUI.py
+++ Calculator_GUI.py
@@ -1,3 +1,13 @@
 # Calculator GUI
 import PySimpleGUI as sg
+import Calculations
 
+layout = [[sg.In(size=(25, 1), enable_events=True, key="-INPUT-")], [sg.Button("OK")]]
+window = sg.Window(title="Calculator", layout=layout, margins=(100, 50))
+
+while True:
+    event, values = window.read()
+    if event == "OK" or even == sg.WIN_CLOSED:
+        break
+
+window.close()
