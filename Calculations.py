b0ed8173e93835ab0713d9b5e6ca5ede2b7d40b8
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2023-03-08 20:18:49 -0700

    Added more operations

--- Calculations.py
+++ Calculations.py
@@ -1,9 +1,12 @@
 # Calculations
+# Project by MozzarellaMonster
+import math
 
 class Calc:
     def __init__(self):
         self.memory = []
     
+    # Arithmetic operations
     def add(self, a, b):
         return a + b
 
@@ -15,8 +18,27 @@ class Calc:
 
     def divide(self, a, b):
         if b == 0:
-            print("Cannot divide by zero!")
+            return "Cannot divide by zero!"
         else:
             return a/b
+        
+    # More complex operations
+    def root(self, a):
+        return math.sqrt(a)
+    
+    def square(self, a):
+        return pow(a, 2)
+
+
+    # Memory management
     def store(self, a):
         self.memory.append(a)
+
+    def delete(self):
+        self.memory.pop()
+
+    def recall(self):
+        return self.memory[-1]
+
+    def clear(self):
+        self.memory.clear()
